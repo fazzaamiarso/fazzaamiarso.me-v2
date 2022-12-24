ff3744bb8c50ba302f4d9843c1382eab80083528
fazzaamiarso <fazzaamiarso@gmail.com>
2022-12-24 23:00:24 +0700

    layout hompage

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        manrope: [
+          "ManropeVariable",
+          "Manrope",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
