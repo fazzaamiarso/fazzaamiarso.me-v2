8befb325d9bd518c66a8b9e7d1950fb0781ce41e
fazzaamiarso <fazzaamiarso@gmail.com>
2022-12-28 22:01:07 +0700

    add projects markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,21 +11,32 @@ import { toString } from "hast-util-to-string";
 
 // https://astro.build/config
 import react from "@astrojs/react";
+import { remarkReadingTime } from "./remarkPlugins.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://fazzaamiarso.me",
-  integrations: [tailwind(), mdx({
-    extendPlugins: true,
-    rehypePlugins: [[rehypeAutolinkHeadings, {
-      behavior: "before",
-      group: () => {
-        return h("div.heading-group");
-      },
-      content: node => {
-        return [h("span.sr-only", `go to ${toString(node)} section`)];
-      }
-    }]]
-  }), react()]
+  integrations: [
+    tailwind(),
+    mdx({
+      extendPlugins: true,
+      remarkPlugins: [remarkReadingTime],
+      rehypePlugins: [
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "before",
+            group: () => {
+              return h("div.heading-group");
+            },
+            content: (node) => {
+              return [h("span.sr-only", `go to ${toString(node)} section`)];
+            },
+          },
+        ],
+      ],
+    }),
+    react(),
+  ],
 });
