bbb28f60a58d6a625797dfb23b2037a1dff4a263
fazzaamiarso <fazzaamiarso@gmail.com>
2023-01-06 02:31:43 +0700

    improve accessibility and add animation to menu button

--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -1,4 +1,4 @@
-import { Menu } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 import clsx from "clsx";
 import { navigations } from "common";
 import { Fragment } from "react";
@@ -12,31 +12,53 @@ export const MobileNav = ({}) => {
             <span className='sr-only'>Navigation menu</span>
             <BarSVG />
           </Menu.Button>
-          <Menu.Items
-            as='div'
-            className='fixed inset-0 z-50 flex flex-col bg-bg p-8 dark:bg-bgDark'>
-            <button onClick={close} className='ml-auto'>
-              <XmarkSVG />
-            </button>
-            <ul className='space-y-8'>
-              {navigations.map((nav) => {
-                return (
-                  <Menu.Item key={nav.href} as='li' className='w-full'>
-                    {({ active }) => (
-                      <a
-                        href={nav.href}
-                        className={clsx(
-                          active && "text-primary",
-                          "inline-block w-full py-2 font-semibold"
-                        )}>
-                        {nav.name}
-                      </a>
-                    )}
-                  </Menu.Item>
-                );
-              })}
-            </ul>
-          </Menu.Items>
+          <Transition
+            as={Fragment}
+            enter='transition ease-out duration-100'
+            enterFrom='transform opacity-0 scale-95'
+            enterTo='transform opacity-100 scale-100'
+            leave='transition ease-in duration-75'
+            leaveFrom='transform opacity-100 scale-100'
+            leaveTo='transform opacity-0 scale-95'>
+            <Menu.Items
+              as='div'
+              className='fixed inset-0 z-50 flex flex-col bg-bg p-8 dark:bg-bgDark'>
+              <button onClick={close} className='ml-auto'>
+                <span className='sr-only'>close navigation menu</span>
+                <XmarkSVG />
+              </button>
+              <ul className='space-y-8'>
+                {navigations.map((nav, idx) => {
+                  const stagger = idx * 75;
+                  return (
+                    <Transition.Child
+                      as='li'
+                      key={nav.href}
+                      style={{ transitionDelay: `${stagger}ms` }}
+                      enter={"transition ease-out duration-500"}
+                      enterFrom='transform -translate-x-full opacity-20'
+                      enterTo='transform translate-x-0 opacity-100'
+                      leave='transition ease-in duration-75'
+                      leaveFrom='transform opacity-100 scale-100'
+                      leaveTo='transform opacity-0 scale-95'>
+                      <Menu.Item as='div' className='w-full'>
+                        {({ active }) => (
+                          <a
+                            href={nav.href}
+                            className={clsx(
+                              active && "text-primary",
+                              "inline-block w-full py-2 font-semibold"
+                            )}>
+                            {nav.name}
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Transition.Child>
+                  );
+                })}
+              </ul>
+            </Menu.Items>
+          </Transition>
         </Fragment>
       )}
     </Menu>
