e3dfca53b5e5bda3e52c34c933ab6ad075e41692
fazzaamiarso <fazzaamiarso@gmail.com>
2023-01-08 00:51:03 +0700

    fix sitemap http to https

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -6,30 +6,30 @@ export const get = () => {
     body: `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
-        <loc>http://www.fazzaamiarso.me/</loc>
+        <loc>https://www.fazzaamiarso.me/</loc>
         <changefreq>yearly</changefreq>
         <priority>1.0</priority>
      </url>
      <url>
-        <loc>http://www.fazzaamiarso.me/about</loc>
+        <loc>https://www.fazzaamiarso.me/about</loc>
         <changefreq>yearly</changefreq>
      </url>
      <url>
-        <loc>http://www.fazzaamiarso.me/snipppets</loc>
+        <loc>https://www.fazzaamiarso.me/snipppets</loc>
         <changefreq>weekly</changefreq>
      </url>
      <url>
-        <loc>http://www.fazzaamiarso.me/projects</loc>
+        <loc>https://www.fazzaamiarso.me/projects</loc>
         <changefreq>weekly</changefreq>
      </url>
      <url>
-        <loc>http://www.fazzaamiarso.me/blog</loc>
+        <loc>https://www.fazzaamiarso.me/blog</loc>
         <changefreq>weekly</changefreq>
      </url>
      ${markdowns
        .map((item) => {
          return `<url>
-        <loc>http://www.fazzaamiarso.me${item.url}</loc>
+        <loc>https://www.fazzaamiarso.me${item.url}</loc>
         <changefreq>monthly</changefreq>
         <lastmod>${item.frontmatter.updatedAt ?? item.frontmatter.publishedAt}</lastmod>
      </url>`;
